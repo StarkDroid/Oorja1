a36c55eba0909cdf6a4adc168e5db7ae03613427
Trishiraj <gamerprince.exp@gmail.com>
2018-04-17 11:49:18 +0530

    Source code of Oorja app, Changelog: - Added Geolocation - Added map marker - identification of Lat/Lng possible in logcat - Added search bar - Added Auto complete places/suggestions
    TO DO:
    - Add map types
    - Implement new UI control for activities
    - Integrate SE/WE calculator
    - Polylines area calculation
    - Custom info window

---
+++ app/src/main/java/niwe/oorja1/SplashScreen.java
@@ -0,0 +1,28 @@
+package niwe.oorja1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
+
+
+
